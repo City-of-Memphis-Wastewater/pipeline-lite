8267b0627f728d51a0416c3b0cac7d875f5486b8
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-01-19 15:02:36 -0600

    dworshak cometh, keyring is out in pipeine_eds 0.4.21

--- src/pipeline_lite/creds.py
+++ src/pipeline_lite/creds.py
@@ -1,18 +1,6 @@
 import pyhabitat as ph
-import keyring
 from pipeline_eds.security_and_config import SecurityAndConfig as SC
 from pipeline_eds.security_and_config import ForcePrompt
-
-# Replace these with the actual service and user names your library uses
-service = "pipeline-eds-api-Stiles" 
-user = "operator" 
-print(f"WSL Keyring value: {keyring.get_password(service, "username")}")
-print(f"WSL Keyring value: {keyring.get_password(service, "password")}")
-
-service = "pipeline-eds-api-Maxson" 
-user = "admin" 
-print(f"WSL Keyring value: {keyring.get_password(service, "username")}")
-print(f"WSL Keyring value: {keyring.get_password(service, "password")}")
 
 
 print(f"ph.interactive_terminal_is_available() = {ph.interactive_terminal_is_available()}")
