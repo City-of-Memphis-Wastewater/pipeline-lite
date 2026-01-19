c2a44d200352377db4c965b843a2983c761276c8
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-01-18 23:22:12 -0600

    increase version of pipeline_eds to 0.4.19, to test forcing logic for prompt_for_value()

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is (read: strives to be) based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
+
+---
+
+## [0.1.3] – 2026-01-18
+### Changed:
+- Change minimum version of pipeline_eds to 0.4.19
--- src/pipeline_lite/creds.py
+++ src/pipeline_lite/creds.py
@@ -1,3 +1,4 @@
+import pyhabitat as ph
 import keyring
 # Replace these with the actual service and user names your library uses
 service = "pipeline-eds-api-Stiles" 
@@ -9,3 +10,11 @@ service = "pipeline-eds-api-Maxson"
 user = "admin" 
 print(f"WSL Keyring value: {keyring.get_password(service, "username")}")
 print(f"WSL Keyring value: {keyring.get_password(service, "password")}")
+
+
+print(f"ph.interactive_terminal_is_available() = {ph.interactive_terminal_is_available()}")
+print(f"ph.tkinter_is_available() = {ph.tkinter_is_available()}")
+print(f"ph.web_browser_is_available() = {ph.web_browser_is_available()}")
+
+from pipeline_eds.security_and_config import SecurityAndConfig as SC
+SC.prompt_for_value(prompt_message = "prompt_message", force_tkinter=True)
