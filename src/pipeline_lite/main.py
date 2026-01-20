bc9cfb77fda7a6f8417ff60226eb5e625b7c2184
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-01-19 18:55:37 -0600

    call both stiles and maxson

--- src/pipeline_lite/main.py
+++ src/pipeline_lite/main.py
@@ -7,13 +7,16 @@ from pipeline_eds.api.eds.soap.client import ClientEdsSoap
 ceds = ClientEdsSoap() # u_eds, p_eds
 #crjn = ClientRjn() # u_rjn, p_rjn
 
-plant_name = "Stiles"
-#plant_name = "Maxson"
-#tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Maxson", idcs = ["m100fi"])
-tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Stiles", idcs = ["I-0300A"])
-#print(f"tabular_data = {tabular_data}")
+# --- plant_name = "Maxson"
+tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Maxson", idcs = ["m100fi"])
 data = ceds.get_tabular_as_dict(tabular_data)
+for entry in data:
+    print(f"Time: {entry['timestamp']} | Value: {entry.get('M100FI')} | Quality: {entry.get('M100FI_quality')}")
 
-#print(f"data = {data}")
+
+
+# --- plant_name = "Stiles"
+tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Stiles", idcs = ["I-0300A"])
+data = ceds.get_tabular_as_dict(tabular_data)
 for entry in data:
     print(f"Time: {entry['timestamp']} | Value: {entry.get('I-0300A')} | Quality: {entry.get('I-0300A_quality')}")
