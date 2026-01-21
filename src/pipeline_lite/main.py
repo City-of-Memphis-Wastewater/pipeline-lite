acce57dcfe6332b279467de55ee73702b8ef69b4
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-01-20 18:56:10 -0600

    increase pipeline-eds to 0.4.27, with refactored ClientRjn

--- src/pipeline_lite/main.py
+++ src/pipeline_lite/main.py
@@ -9,25 +9,46 @@ import dworshak_access as da
 ceds = ClientEdsSoap() # u_eds, p_eds
 crjn = ClientRjn() # u_rjn, p_rjn
 
-# --- plant_name = "Maxson"
+# ------ Calling data from our two plant APIs (EDS) -----------
+"""
+## --- plant_name = "Maxson"
 tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Maxson", idcs = ["m100fi"])
 data = ceds.get_tabular_as_dict(tabular_data)
 for entry in data:
     print(f"Time: {entry['timestamp']} | Value: {entry.get('M100FI')} | Quality: {entry.get('M100FI_quality')}")
 
 
-
-# --- plant_name = "Stiles"
+## --- plant_name = "Stiles"
 tabular_data = ceds.soap_api_iess_request_tabular(plant_name = "Stiles", idcs = ["I-0300A"])
 data = ceds.get_tabular_as_dict(tabular_data)
 for entry in data:
     print(f"Time: {entry['timestamp']} | Value: {entry.get('I-0300A')} | Quality: {entry.get('I-0300A_quality')}")
+"""
+# ------ Sending to a third party, RJN -----------
 
 service = "pipeline-rjn-clarity"
-rjn_base_url = da.get_secret(service = service, item = "url")
-rjn_client_id = da.get_secret(service = service, item = "username")
-rjn_password = da.get_secret(service = service, item = "password")
+TEST = da.get_secret(service = service, item = "test", fail = True)
+print(f"TEST = {TEST}")
 
-session = ClientRjn.login_to_session(api_url = rjn_base_url,
+
+rjn_base_url = da.get_secret(service = service, item = "url", fail = True)
+print(f"rjn_base_url = {rjn_base_url}")
+rjn_client_id = da.get_secret(service = service, item = "username", fail = True)
+rjn_password = da.get_secret(service = service, item = "password", fail = True)
+
+bool_session = crjn.login_to_session(api_url = rjn_base_url,
     client_id = rjn_client_id,
     password = rjn_password)
+
+print(f"bool_session = {bool_session}")
+
+"""
+bool_transmit = crjn.send_data_to_rjn(session = session, 
+                      base_url =  rjn_base_url, 
+                      project_id=None, 
+                      entity_id=None, 
+                      timestamps=None, 
+                      values=None  )
+print(f"bool_transmit = {bool_transmit}")
+                      
+                      """
